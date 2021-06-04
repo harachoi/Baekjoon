c8fee52f8072d52ad0adf48443ffcedcec090409
Hara Choi <hara77227722@gmail.com>
2021-06-04 12:38:11 +0900

    Create 1010_다리_놓기.java

---
+++ Dynamic Programming/1010_다리_놓기.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[][] dp = new int[30][30];
+        int t = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 30; i++) {
+            dp[i][i] = 1;
+            dp[i][0] = 1;
+        }
+
+        for (int i = 2; i < 30; i++) {
+            for (int j = 1; j < 30; j++) {
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+            }
+        }
+
+        for(int i = 0; i < t; i++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int m = Integer.parseInt(st.nextToken());
+            sb.append(dp[m][n]).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
