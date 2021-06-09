fab186b869712f3dcc0f67c44928f4702982954c
Hara Choi <hara77227722@gmail.com>
2021-06-09 13:46:55 +0900

    Create 11660_구간_합_구하기5.java

---
+++ Dynamic Programming/11660_구간_합_구하기5.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] map = new int[n + 1][n + 1];
+        int[][] dp = new int[n+1][n+1];
+
+        //build map
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j =  1; j <= n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp [i-1][j-1] + map[i][j];
+            }
+        }
+
+        //simulation
+        for (int t = 0; t < m; t++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+
+            int sum = dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1] ;
+            sb.append(sum).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
