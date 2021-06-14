88e150668d49340a55b4d29be908c8ffa970cad0
Hara Choi <hara77227722@gmail.com>
2021-06-14 20:48:27 +0900

    Create 3046_R2.java

---
+++ Math/3046_R2.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int r1 = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+
+        System.out.println(s * 2 - r1);
+    }
+}
