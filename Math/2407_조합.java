df432e134d13beb3661363404983a7d10d7db83b
Hara Choi <hara77227722@gmail.com>
2021-06-14 20:17:58 +0900

    Create 2407_조합.java

---
+++ Math/2407_조합.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        BigInteger a = BigInteger.ONE;
+        BigInteger b = BigInteger.ONE;
+
+        for (int i = 0; i < m; i++) {
+            a = a.multiply(new BigInteger(String.valueOf(n-i)));
+            b = b.multiply(new BigInteger(String.valueOf(i + 1)));
+        }
+        System.out.println(a.divide(b));
+    }
+}
