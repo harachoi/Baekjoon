a722e3dc7717fae07c971d928b4d9081fb970242
Hara Choi <hara77227722@gmail.com>
2021-06-26 16:34:04 +0900

    Create 20053_최소,최대2.java

---
+++ Simulation/20053_최소,최대2.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            int n = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int max = Integer.MIN_VALUE;
+            int min = Integer.MAX_VALUE;
+            for (int i = 0; i < n; i++) {
+                int num = Integer.parseInt(st.nextToken());
+                max = Math.max(max, num);
+                min = Math.min(min, num);
+            }
+            sb.append(min + " " + max).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
